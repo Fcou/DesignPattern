b166f106da6d88200ebc6ad1b5b49275f2151ae2
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-02-20 16:48:02 +0800

    适配器模式

--- Adapter/main.go
+++ Adapter/main.go
@@ -3,6 +3,8 @@
 Adapter模式，使得原本由于原本接口不兼容而不能一起工作的那些类可以一起工作。
 
 已存在两个不同的接口，在不修改两个接口的前提下，新增一个适配器类，在两个类中统一接口
+这在调用第三方库的过程中，非常常见，通常自己按照接口创建一个新struct，具体变量，具体方法，自己按照需求写好
+最后创建接口，执行接口方法
 */
 
 package main
@@ -35,8 +37,8 @@ func (t *transformer) on220vCharge() {
 func main() {
 	var c Charge220V
 	c = &transformer{
-		charge: &iphoneCharger{},
+		charge: &iphoneCharger{}, //赋值为我们需要的类
 	}
 
-	c.on220vCharge()
+	c.on220vCharge() //通过接口.方法 调用我们需要类的方法
 }
