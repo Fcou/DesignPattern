59a16d575630c2b9b47380c006c313ec9a6fd79f
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-03-10 20:19:35 +0800

    单例模式：添加说明

--- Singleton/other.go
+++ Singleton/other.go
@@ -17,7 +17,7 @@ func GetInstance(name string) *Instance {
 	if instance == nil {
 		lock.Lock()
 		defer lock.Unlock()
-		if instance == nil {
+		if instance == nil { //防止第一个go程创建实例解锁后，有之前排队阻塞go程，醒来再次创建实例
 			instance = &Instance{Name: name}
 		}
 	}
